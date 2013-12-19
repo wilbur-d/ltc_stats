3cf779190f27b8e7c76554bb7d62ed8ca654b7d3
Tosh Lyons <tosh.lyons@gmail.com>
2013-12-18 19:17:39 -0600

    setup logger configuration

--- stats_app/settings.py
+++ stats_app/settings.py
@@ -1,8 +1,12 @@
+import os
+import logging
+
+# API settings
 API_URL = "https://btc-e.com/api/2/ltc_usd/ticker"
 MINING_API_URL = ""
 TRADE_API_URL = "https://btc-e.com/api/2/ltc_usd/trades"
 
-import os
+# Project settings
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 DB_PATH = os.path.join(PROJECT_ROOT, '../ticker.db')
 
@@ -13,7 +17,11 @@ DATABASE = {'drivername': 'sqlite',  # postgresql
             # 'username': '',
             # 'password': ''}
 
+# Logging settings
+LOG_FILE = os.path.join(PROJECT_ROOT, "logs", "ltc.log")
+logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
 
+# local settings override
 try:
     from local_settings import *
 except ImportError, exp:
