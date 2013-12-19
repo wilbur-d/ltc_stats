89693a4e1f5ee6d48531d944689230b5ca36fcab
Tosh Lyons <tosh.lyons@gmail.com>
2013-12-18 19:33:26 -0600

    added basic logging to main.py
    super generic logging

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 #! /usr/bin/env python
 
+import logging
+import sys
+
 from stats_app.ticker_store import TickerStore, MiningHistoryStore, TradeStore
 from stats_app.settings import API_URL, MINING_API_URL, TRADE_API_URL
 
+log = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    tradestore = TradeStore(TRADE_API_URL)
-    tradestore.save()
+    try:
+        mstore = MiningHistoryStore(MINING_API_URL)
+        mstore.save()
+    except:
+        e = sys.exc_info()[0]
+        log.error("Unable to save MiningHistory. %s" % e)
 
-    tstore = TickerStore(API_URL)
-    tstore.save()
+    try:
+        tstore = TickerStore(API_URL)
+        tstore.save()
+    except:
+        e = sys.exc_info()[0]
+        log.error("Unable to save Ticker. %s" % e)
 
-    mstore = MiningHistoryStore(MINING_API_URL)
-    mstore.save()
+    try:
+        tradestore = TradeStore(TRADE_API_URL)
+        tradestore.save()
+    except:
+        e = sys.exc_info()[0]
+        log.error("Unable to save Trade. %s" % e)
