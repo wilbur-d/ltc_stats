69c5a020cdf9833f13400b90e154246bab085a80
Mark Scala <markscala@gmail.com>
2013-12-23 14:28:28 -0600

    uncommented some code in main

--- main.py
+++ main.py
@@ -9,27 +9,27 @@ from stats_app.settings import API_URL, POOL, TRADE_API_URL
 log = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    # try:
-    #     mstore = MiningHistoryStore(POOL)
-    #     mstore.save()
-    # except:
-    #     e = sys.exc_info()[0]
-    #     log.error("Unable to save MiningHistory. %s" % e)
-    #     raise
+    try:
+        mstore = MiningHistoryStore(POOL)
+        mstore.save()
+    except:
+        e = sys.exc_info()[0]
+        log.error("Unable to save MiningHistory. %s" % e)
+        raise
 
-    # try:
-    #     tstore = TickerStore(API_URL)
-    #     tstore.save()
-    # except:
-    #     e = sys.exc_info()[0]
-    #     log.error("Unable to save Ticker. %s" % e)
+    try:
+        tstore = TickerStore(API_URL)
+        tstore.save()
+    except:
+        e = sys.exc_info()[0]
+        log.error("Unable to save Ticker. %s" % e)
 
-    # try:
-    #     tradestore = TradeStore(TRADE_API_URL)
-    #     tradestore.save()
-    # except:
-    #     e = sys.exc_info()[0]
-    #     log.error("Unable to save Trade. %s" % e)
+    try:
+        tradestore = TradeStore(TRADE_API_URL)
+        tradestore.save()
+    except:
+        e = sys.exc_info()[0]
+        log.error("Unable to save Trade. %s" % e)
 
     try:
         gpustore = GPUStore('stats')
@@ -38,9 +38,9 @@ if __name__ == "__main__":
         e = sys.exc_info()[0]
         log.error("Unable to save GPU stats. %s" % e)
 
-        #    try:
-    poolstore = MinerPoolStore('stats')
-    poolstore.save()
-    # except:
-    #     e = sys.exc_info()[0]
-    #     log.error("Unable to save Miner pool stats. %s" % e)
+    try:
+        poolstore = MinerPoolStore('stats')
+        poolstore.save()
+    except:
+        e = sys.exc_info()[0]
+        log.error("Unable to save Miner pool stats. %s" % e)
